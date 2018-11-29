befcc9e362ee60045590087eb467fd1a7b410898
Tony D <tdipentino@axway.com>
2018-11-29 10:38:21 -0700

    updating dir

--- models/bison.js
+++ models/bison.js
@@ -26,133 +26,13 @@ var Model = Arrow.createModel('bison', {
         "weight": {
             "type": "string"
         },
-        "weight_rank": {
-            "type": "number",
-            "default": "0"
-        },
-        "is_varsity": {
-            "type": "number",
-            "default": "0"
-        },
-        "match_points": {
-            "type": "number",
-            "default": "0"
-        },
-        "team_points": {
-            "type": "number",
-            "default": "0"
-        },
-        "wins": {
-            "type": "number",
-            "default": "0"
-        },
-        "loses": {
-            "type": "number",
-            "default": "0"
-        },
-        "fall": {
-            "type": "number",
-            "default": "0"
-        },
-        "major_decision": {
-            "type": "number",
-            "default": "0"
-        },
-        "decision": {
-            "type": "number",
-            "default": "0"
-        },
-        "forfeit": {
-            "type": "number",
-            "default": "0"
-        },
-        "tech_fall": {
-            "type": "number",
-            "default": "0"
-        },
-        "take_down": {
-            "type": "number",
-            "default": "0"
-        },
-        "near_fall_2": {
-            "type": "number",
-            "default": "0"
-        },
-        "near_fall_3": {
-            "type": "number",
-            "default": "0"
-        },
-        "e1": {
-            "type": "number",
-            "default": "0"
-        },
-        "reversal": {
-            "type": "number",
-            "default": "0"
-        },
-        "is_jv": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_match_points": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_team_points": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_wins": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_loses": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_fall": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_major_decision": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_decision": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_forfeit": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_tech_fall": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_take_down": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_near_fall_2": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_near_fall_3": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_e1": {
-            "type": "number",
-            "default": "0"
-        },
-        "jv_reversal": {
-            "type": "number",
-            "default": "0"
+        "team": {
+            "type": "string",
+            "default": "v"
         },
         "year": {
             "type": "number",
-            "default": "2018"
+            "default": "2019"
         }
     },
     "connector": "appc.arrowdb",
